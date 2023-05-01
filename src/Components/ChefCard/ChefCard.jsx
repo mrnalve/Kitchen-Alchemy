0724ea1bcb68dbf67408759befc0e452040963ae
Md Alve <mdalve1091@gmail.com>
2023-05-02 02:00:20 +0600

    show all chef cards

--- src/Components/ChefCard/ChefCard.jsx
+++ src/Components/ChefCard/ChefCard.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
 
-const ChefCard = () => {
-    return (
-        <div>
-            Hello
+const ChefCard = ({ chef }) => {
+    const {picture} = chef
+  return (
+    <div className="card card-compact w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src={picture}
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Buy Now</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ChefCard;
--- src/Components/ChefSection/ChefSection.jsx
+++ src/Components/ChefSection/ChefSection.jsx
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from "react";
 import ChefCard from "../ChefCard/ChefCard";
 
 const ChefSection = () => {
-  const [chefs, setChefs] = useState();
+  const [chefs, setChefs] = useState([]);
     useEffect(()=>{
         fetch('http://localhost:5000/chefs')
         .then(res => res.json())
         .then(data => setChefs(data))
     },[])
-  return <div>
+  return <div className="grid grid-cols-1 md:grid-cols-3">
     {
         chefs.map(chef => <ChefCard key={chef.id} chef={chef}></ChefCard>)
     }
