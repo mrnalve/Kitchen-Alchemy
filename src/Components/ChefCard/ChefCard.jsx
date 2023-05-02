db19694cdb3e7c248a88a8958a96a1b88909ff29
Md Alve <mdalve1091@gmail.com>
2023-05-02 11:00:37 +0600

    chef card complete

--- src/Components/ChefCard/ChefCard.jsx
+++ src/Components/ChefCard/ChefCard.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 
 const ChefCard = ({ chef }) => {
-    const {picture} = chef
+    const {picture, name, years_of_experience, number_of_recipes, likes} = chef
   return (
-    <div className="card card-compact w-96 bg-base-100 shadow-xl">
-      <figure>
+    <div className="card card-compact w-96 h-[500px] bg-base-100 shadow-xl m-3">
+      <figure className="w-full">
         <img
+         className="w-full h-full"
           src={picture}
           alt="Shoes"
         />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <h2 className="card-title text-2xl">{name}</h2>
+        <p className="text-[16px] font-serif">Years of experience: {years_of_experience}</p>
+        <p className="text-[16px] font-serif">Number of recipes: {number_of_recipes}</p>
+        <p className="text-[16px] font-serif">Likes: {likes}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+          <button className="btn btn-primary">View Recipe</button>
         </div>
       </div>
     </div>
--- src/Components/ChefSection/ChefSection.jsx
+++ src/Components/ChefSection/ChefSection.jsx
@@ -3,16 +3,21 @@ import ChefCard from "../ChefCard/ChefCard";
 
 const ChefSection = () => {
   const [chefs, setChefs] = useState([]);
-    useEffect(()=>{
-        fetch('http://localhost:5000/chefs')
-        .then(res => res.json())
-        .then(data => setChefs(data))
-    },[])
-  return <div className="grid grid-cols-1 md:grid-cols-3">
-    {
-        chefs.map(chef => <ChefCard key={chef.id} chef={chef}></ChefCard>)
-    }
-  </div>;
+  useEffect(() => {
+    fetch("http://localhost:5000/chefs")
+      .then((res) => res.json())
+      .then((data) => setChefs(data));
+  }, []);
+  return (
+    <div className="my-20">
+      <h2 className="font-bold text-4xl text-yellow-500 text-center underline">Choose your chefs here</h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 mt-7">
+        {chefs.map((chef) => (
+          <ChefCard key={chef.id} chef={chef}></ChefCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ChefSection;
